705576d1a1e356d883044c8540108c37681272db
Σεραφείμ Λάμπρου < >
2017-01-08 21:56:24 +0200

    searchBar - First part

---
+++ src/components/search_bar.js
@@ -0,0 +1,48 @@
+//search-bar component
+
+import React, {Component} from 'react';
+//the same as const Component = React.Component
+
+//const SearchBar = () => {
+//  return <input/> //React.createElement
+//}; //it is a functional component. There is also a class component
+
+
+//We will declare a class component. Class components are used
+//when we need to check for the state of a component
+
+
+//Each class based component has its own 'state' property
+//When state changes, the component automatically re-renders
+//All of its children re-render too
+class SearchBar extends Component {
+  constructor(props){
+    super(props); //calls the constructor of the extended function(Component)
+
+    this.state = {term: ''};//initialize state propeerty.
+    //State can contain multiple properties.
+    //Here we care about the term the user endered
+  }
+
+    render(){ //method so it can renders itself. it is still a function despite the syntax
+      return (
+        <div>
+          <input
+            value={this.state.term}
+            onChange={event => this.setState({ term: event.target.value })}/>
+
+        </div>
+      );
+      //onChange is an out of the box event. It is a default react property.
+      //setState changes the state of the instance of a class
+      //Whenever we reference a javascript variable, we wrap it around {}
+    };
+};
+  //onInputChange(event) { //We define an event handler
+//    //we use the event input to access the value of the input
+//    console.log(event.target.value);
+//  }
+//}
+
+//export my code for use in other files
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,38 @@
+// Create a new component. This component
+//should produce some HTML
+
+//ES6 supports javascripts modules
+//That means that we can reference other files
+//in order to execute the code they contain
+//In our case react is installed in the node_modules
 import React from 'react';
+//React is diverged into two parts
+//React knows how to nest elements together
+//ReactDom knows how to render components in the DOM
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar'
 
-import App from './components/app';
-import reducers from './reducers';
+//key for youtube
+const API_KEY = 'AIzaSyAIhqy8YY1T6ZigO-RIqhc84ujFCjsHR1o';
+//const is like declaring a variable that
+//will not change
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//arrow is another symbol for function
+const App = () => { //This is a class of component, not an instance
+  return (<div>
+    <SearchBar />
+  </div>
+);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+  //this syntax is jsx - a dialect of js
+  //webpack and bable will transpile it into core javascript
+  //jsx cannot interpreted by browser
+  //in order to see transpiled code go to www.babel.io
+};
+
+ReactDOM.render(<App />, document.querySelector('.container')); //we need to reneder instance of components, not classes
+                          //the tag <App /> creates an instance of the class App
+                          //second argument defines the target container
+
+//Take this component's generated HTML
+//and put it on the page (in the DOM)
